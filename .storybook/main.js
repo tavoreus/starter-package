bdd4ae906ed3211307dbeaffaf50c0d258555628
tavoreus <tavo_reus@hotmail.com>
2023-05-28 19:39:32 +0100

    testing with storybook babel config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,3 @@
-const path = require("path");
-
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -20,11 +18,5 @@ module.exports = {
     },
   ],
   framework: "@storybook/react",
-  staticDirs: [
-    {
-      directory: path.resolve(__dirname, "../src"),
-      staticPath: "./src",
-      watch: true,
-    },
-  ],
+  staticDirs: ["../public"],
 };
