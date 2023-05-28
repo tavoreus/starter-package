52f161519b22ed54940b55278fcdc9aca152f332
tavoreus <tavo_reus@hotmail.com>
2023-05-28 20:21:22 +0100

    build

--- dist/index.js
+++ dist/index.js
@@ -4,12 +4,6 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "ArticleCard", {
-  enumerable: true,
-  get: function get() {
-    return _ArticleCard["default"];
-  }
-});
 Object.defineProperty(exports, "Button", {
   enumerable: true,
   get: function get() {
@@ -24,4 +18,3 @@ Object.defineProperty(exports, "Tag", {
 });
 var _Button = _interopRequireDefault(require("./elements/Button/Button"));
 var _Tag = _interopRequireDefault(require("./elements/Tag/Tag"));
-var _ArticleCard = _interopRequireDefault(require("./components/Cards/ArticleCard"));
