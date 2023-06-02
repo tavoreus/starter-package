b93a20e6e2a4255d49c9955af406e7351fcd38f5
tavoreus <tavo_reus@hotmail.com>
2023-06-02 20:48:53 +0100

    asd

--- src/themes/tailwind.m.config.js
+++ src/themes/tailwind.m.config.js
@@ -1,45 +1,43 @@
-/** @type {import('tailwindcss').Config} */
-
 // import addPlugin from "tailwindcss/plugin";
 // import aspectRatioPlugin from "@tailwindcss/aspect-ratio";
 
-const BASE_FONT_SIZE_PX = 10;
+// const BASE_FONT_SIZE_PX = 10;
 
-const noop = (val) => val;
-const unitToPx = (val) => `${val}px`;
-const unitToRem = (val) => `${val}rem`;
-const pxToRem = (val) => val / BASE_FONT_SIZE_PX;
-const pxUnitToRem = (val) => unitToRem(pxToRem(val));
+// const noop = (val) => val;
+// const unitToPx = (val) => `${val}px`;
+// const unitToRem = (val) => `${val}rem`;
+// const pxToRem = (val) => val / BASE_FONT_SIZE_PX;
+// const pxUnitToRem = (val) => unitToRem(pxToRem(val));
 
-const createScale = ({
-  min = 0,
-  max = 100,
-  steps = 1,
-  formatVal = noop,
-  formatKey = noop,
-}) => {
-  const limit = Math.round((max - min) / steps);
-  const scale = [...new Array(limit + 1)].map((_, i) => min + i * steps);
+// const createScale = ({
+//   min = 0,
+//   max = 100,
+//   steps = 1,
+//   formatVal = noop,
+//   formatKey = noop,
+// }) => {
+//   const limit = Math.round((max - min) / steps);
+//   const scale = [...new Array(limit + 1)].map((_, i) => min + i * steps);
 
-  return scale.reduce((prev, curr) => {
-    const key = String(formatKey(curr));
-    const val = curr === 0 ? curr : formatVal(curr);
-    return { ...prev, [key]: val };
-  }, {});
-};
+//   return scale.reduce((prev, curr) => {
+//     const key = String(formatKey(curr));
+//     const val = curr === 0 ? curr : formatVal(curr);
+//     return { ...prev, [key]: val };
+//   }, {});
+// };
 
-const spacing = {
-  ...createScale({ max: 32, steps: 1, formatVal: pxUnitToRem }),
-  ...createScale({ min: 32, max: 64, steps: 2, formatVal: pxUnitToRem }),
-  ...createScale({ min: 68, max: 128, steps: 4, formatVal: pxUnitToRem }),
-  ...createScale({ min: 136, max: 256, steps: 8, formatVal: pxUnitToRem }),
-  ...createScale({ min: 272, max: 512, steps: 16, formatVal: pxUnitToRem }),
-  ...createScale({ min: 544, max: 1024, steps: 32, formatVal: pxUnitToRem }),
-};
+// const spacing = {
+//   ...createScale({ max: 32, steps: 1, formatVal: pxUnitToRem }),
+//   ...createScale({ min: 32, max: 64, steps: 2, formatVal: pxUnitToRem }),
+//   ...createScale({ min: 68, max: 128, steps: 4, formatVal: pxUnitToRem }),
+//   ...createScale({ min: 136, max: 256, steps: 8, formatVal: pxUnitToRem }),
+//   ...createScale({ min: 272, max: 512, steps: 16, formatVal: pxUnitToRem }),
+//   ...createScale({ min: 544, max: 1024, steps: 32, formatVal: pxUnitToRem }),
+// };
 
 module.exports = {
   utils: {
-    pxRem: pxUnitToRem,
+    // pxRem: pxUnitToRem,
   },
   preset: {
     theme: {
@@ -103,10 +101,10 @@ module.exports = {
         },
       },
       extend: {
-        spacing,
-        zIndex: {
-          behind: -1,
-        },
+        // spacing,
+        // zIndex: {
+        //   behind: -1,
+        // },
       },
     },
     plugins: [
